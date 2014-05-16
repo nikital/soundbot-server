aa51a971dcfd704e8a8d32c611f926dc169652c5
Nikita Leshenko <nikita.leshenko@gmail.com>
2014-05-16 13:05:25 +0300

    If there is an environment (front) camera, use it

--- public/js/camera.js
+++ public/js/camera.js
@@ -1,28 +1,52 @@
-getUserMedia({video: true, audio: false}, function(stream) {
-    var pc = null;
+MediaStreamTrack.getSources(gotSources);
 
-    var session = prompt('Session name:', 'nik');
-    var signaling = new Signaling('ws://' + window.location.host + '/sdp/offer', session);
+function gotSources(sources)
+{
+    var videoSources = sources.filter(function(s) { return s.kind == 'video'; });
+    if (videoSources.length == 0) {
+        console.log('No video sources');
+    }
 
-    signaling.$dispatcher.on('message', function(e, message) {
-        if (message.get_offer) {
-            pc = RTCPeerConnection(WebRTC.peerConnectionConfig);
+    var environmentSourceId = 0;
 
-            pc.onicecandidate = function(e) {
-                if (e.candidate) {
-                    signaling.signal({ice: e.candidate});
-                }
-            };
-
-            pc.addStream(stream);
-            pc.createOffer(function(desc) {
-                pc.setLocalDescription(desc);
-                signaling.signal({sdp: desc});
-            });
-        } else if (message.sdp && pc) {
-            pc.setRemoteDescription(new RTCSessionDescription(message.sdp));
-        } else if (message.ice && pc) {
-            pc.addIceCandidate(new RTCIceCandidate(message.ice));
+    for (var i = 0; i < videoSources.length; ++i) {
+        if (videoSources[i].facing == 'environment') {
+            environmentSourceId = i;
+            break;
         }
+    }
+
+    startCamera(videoSources[environmentSourceId].id);
+}
+
+function startCamera(videoSourceId)
+{
+    getUserMedia({video: {optional: [{sourceId: videoSourceId}]}, audio: false}, function(stream) {
+        var pc = null;
+
+        var session = prompt('Session name:', 'nik');
+        var signaling = new Signaling('ws://' + window.location.host + '/sdp/offer', session);
+
+        signaling.$dispatcher.on('message', function(e, message) {
+            if (message.get_offer) {
+                pc = RTCPeerConnection(WebRTC.peerConnectionConfig);
+
+                pc.onicecandidate = function(e) {
+                    if (e.candidate) {
+                        signaling.signal({ice: e.candidate});
+                    }
+                };
+
+                pc.addStream(stream);
+                pc.createOffer(function(desc) {
+                    pc.setLocalDescription(desc);
+                    signaling.signal({sdp: desc});
+                });
+            } else if (message.sdp && pc) {
+                pc.setRemoteDescription(new RTCSessionDescription(message.sdp));
+            } else if (message.ice && pc) {
+                pc.addIceCandidate(new RTCIceCandidate(message.ice));
+            }
+        });
     });
-});
+}
